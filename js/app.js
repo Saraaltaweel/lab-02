cca54ddcab6b8b97e9045243c1e98fc504db5999
Saraaltaweel <s.bata97.st@gmail.com>
2021-03-27 22:00:31 +0300

    changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 'use strict';
+
+let keyArr = [];
+let uniqueArray=[];
 function Horns(animal){
     this.title=animal.title;
     this.image_url=animal.image_url;
@@ -8,7 +11,12 @@ function Horns(animal){
    
 
 }
-
+Horns.prototype.dropList = function(){
+  keyArr.push(this.keyword);
+  uniqueArray = [...new Set(keyArr)];
+  console.log(uniqueArray);
+  
+};
 
 const ajaxItem={
     method: 'get',
@@ -19,27 +27,40 @@ const ajaxItem={
 $.ajax('data/page-1.json', ajaxItem).then((data) => {
   data.forEach(animal => {
     let item = new Horns(animal);
+    
     item.renderClone();
+    item.dropList();
+
+    
   });
+  uniqueArray.forEach(element =>{
+    $('select').append(`<option value="${element}">${element}</option>`);
+  })
+
 });
+ 
 
 Horns.prototype.renderClone=function(){
-  let allOption=$('<option></option>').text(this.keyword);
-  $('select').append(allOption);
+  // uniqueArray.forEach(element =>{
+  //   $('select').append(`<option value="${element}">${element}</option>`);
+  // })
+  // let allOption=$('<option></option>').text(uniqueArray);
+  // $('select').append(allOption);
 let clone=$("#hornInfo").clone();
-$('main').append(clone);
+
 clone.find("h2").text(this.title);
 clone.find("img").attr("src",this.image_url);
 clone.find("p").text(this.description);
 clone.attr("class", this.keyword);
 clone.attr("id", this.keyword);
 clone.find("h3").text(this.horns);
-
+$('main').append(clone);
 }
+  // )}
 
 $(document).ready(function(){
 
-  $('select').click('click',function(){
+  $('select').click('change',function(){
     let selectItem = this.value;
     $('section').hide();
     $(`.${selectItem}`).show();
